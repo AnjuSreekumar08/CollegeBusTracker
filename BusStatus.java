fb52655ceaa56317bac0bbe534c2f21164171763
vigneshmh212 <61729965+vigneshmh212@users.noreply.github.com>
2020-12-28 19:29:39 +0530

    Busstatus

---
+++ BusStatus.java
@@ -0,0 +1,16 @@
+package com.example.collegebustrack;
+
+public class BusStatus {
+
+    private String status;
+
+    public BusStatus(){}
+
+    public BusStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
